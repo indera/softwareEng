f7deed95b6ed21e387721001f7f4a2e6eae9ae92
lucasarango <lucasarango@ufl.edu>
2014-02-09 16:09:33 -0500

    Updated JSON funct

--- raspberrypi/Temperature.py
+++ raspberrypi/Temperature.py
@@ -4,30 +4,32 @@
 
 import RPi.GPIO as GPIO
 import time
+import json
+import pickle
 
-LED1_GPIO_PIN = 18
-LED2_GPIO_PIN = 25
-BUTTON_GPIO_PIN = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(BUTTON_GPIO_PIN, GPIO.IN)
-GPIO.setup(LED1_GPIO_PIN, GPIO.OUT)
-GPIO.setup(LED2_GPIO_PIN, GPIO.OUT)
+# LED1_GPIO_PIN = 18
+# LED2_GPIO_PIN = 25
+# BUTTON_GPIO_PIN = 17
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(BUTTON_GPIO_PIN, GPIO.IN)
+# GPIO.setup(LED1_GPIO_PIN, GPIO.OUT)
+# GPIO.setup(LED2_GPIO_PIN, GPIO.OUT)
 
-GPIO.output(LED1_GPIO_PIN, GPIO.HIGH)
+# GPIO.output(LED1_GPIO_PIN, GPIO.HIGH)
 
-while True:
-	if GPIO.input(BUTTON_GPIO_PIN):
-		break
+# while True:
+	# if GPIO.input(BUTTON_GPIO_PIN):
+		# break
 
-while GPIO.input(BUTTON_GPIO_PIN):
-	pass
+# while GPIO.input(BUTTON_GPIO_PIN):
+	# pass
 
-GPIO.output(LED1_GPIO_PIN, GPIO.LOW)
-GPIO.output(LED2_GPIO_PIN, GPIO.HIGH)
+# GPIO.output(LED1_GPIO_PIN, GPIO.LOW)
+# GPIO.output(LED2_GPIO_PIN, GPIO.HIGH)
 
-timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
-filename = "".join(["temperaturedata", timestamp, ".log"])
-datafile = open(filename, "w", 1)
+# timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
+# filename = "".join(["temperaturedata", timestamp, ".log"])
+# datafile = open(filename, "w", 1)
 
 while True:
 	tfile = open("/sys/bus/w1/devices/10-000802824e58/w1_slave")
@@ -37,6 +39,9 @@ while True:
 	temperature = float(temperature_data[2:])
 	temperature = temperature / 1000
 	datafile.write(str(temperature) + "\n")
+	data = [ {  'cusEmail' : 'indera@gmail.com', 'raspsDescription' : 'Temperature Sensor', 'temperature' : temperature, 'time': timestamp }]
+	json= json.dumps(data)
+	
 	if GPIO.input(BUTTON_GPIO_PIN):
 		break
 
